5d556bfe1d56dbbda423df14faccf0e71aa8c544
GaelMariani <73817504+GaelMariani@users.noreply.github.com>
2024-03-04 11:32:54 +0100

    Sankey diagramm at the continent et regional scale

--- analyses/map_ratio_geoP_vs_first_author_paper.R
+++ analyses/map_ratio_geoP_vs_first_author_paper.R
@@ -24,19 +24,6 @@ library(ggrepel)
 source(here::here("R", "functions_to_format.R")) # all functions needed to format data
 source(here::here("R", "functions_to_plot.R")) # all functions needed to plot dataw
 
-### ----- LOAD LIBRARIES -----
-library(dplyr)
-library(dbplyr)
-library(R.utils)
-library(RSQLite)
-library(ggplot2)
-library(tidyr)
-library(stringr)
-library(viridis)
-library(rgdal)
-library(countrycode)
-library(broom)
-library(ggrepel)
 
 ### ----- CONNECTION TO THE LATEST VERSION OF THE SQL DATABASE -----
 sqliteDir <- here::here("data/sqlite-databases")
@@ -190,12 +177,12 @@ dbcon <- RSQLite::dbConnect(RSQLite::SQLite(), file.path(sqliteDir, latestVersio
     # --- Data a the country scale for geoparsing
     data_per_country_geoP <- data_1A_geop |> 
       group_by(country_geop, iso_code_geoP) |> 
-      summarise(n_geop = n())
+      summarise(n_geop = n()) 
     
     # --- Data a the country scale for 1 author affiliation
     data_per_country_aff <- data_1A_geop |> 
       group_by(country_aff, iso_code_aff) |> 
-      summarise(n_aff = n())
+      summarise(n_aff = n()) 
     
     # --- Common data at the country scale
     ORO_per_country_common_Aff_Geop <- full_join(data_per_country_aff, data_per_country_geoP, by = c("iso_code_aff" = "iso_code_geoP")) |> 
@@ -237,6 +224,112 @@ dbcon <- RSQLite::dbConnect(RSQLite::SQLite(), file.path(sqliteDir, latestVersio
                            title_size        = NULL,
                            show.legend       = TRUE,
                            name              = "main/map_geoP_vs_affiliation")
+  
+  
+### ----
 
+### ---- PANEL B -----
+  
+  ## --- Format data from the Sankey Diagram
+  
+    # -- Find the corresponding continent and sumamrise
+    ccodes <- raster::ccodes() |>  
+      select(NAME, continent, UNREGION1) |> 
+      mutate(NAME = case_when(NAME == "Micronesia" ~ "Micronesia (Federated States of)",
+                              TRUE ~ NAME)) |> 
+      rename(region = UNREGION1) |> 
+      mutate(NAME = countrycode(sourcevar   = NAME,
+                                origin      = "country.name",
+                                destination = "country.name"),
+             region = case_when(str_detect(region, pattern = "Asia") == TRUE ~ "Asia",
+                                str_detect(region, pattern = "Africa") == TRUE ~ "Africa",
+                                str_detect(region, pattern = "Europe") == TRUE ~ "Europe",
+                                TRUE ~ region))
+      
+  
+    # -- Add the continent to the geoparsed data
+    geop_data_conti <- geop_data_all |> 
+        left_join(ccodes, by = c("country_geop" = "NAME")) |> 
+        rename(continent_geoP = continent,
+               region_geoP    = region) 
+      
+    # -- Add the continent to the 1st author affiliation data
+    data_aff_conti <- data_aff |> 
+      left_join(ccodes, by = c("country_aff" = "NAME")) |> 
+      rename(continent_aff = continent,
+             region_aff    = region)  
 
+    
+    # -- Bind Data
+    data_aff_geoP <- left_join(geop_data_conti, data_aff_conti, by = "analysis_id") |> 
+      replace_na(list(n_aff = 0,
+                      country_aff   = "Unknown",
+                      iso_code_aff  = "Unknown",
+                      continent_aff = "Unknown",
+                      region_aff    = "Unknown")) 
+    
+  ## --- Format data at the continent scale
+  data_aff_geoP_conti <- data_aff_geoP |> 
+    dplyr::select(analysis_id, continent_geoP, continent_aff) |> 
+    group_by(continent_aff, continent_geoP) |> 
+    summarise(value = n()) |> 
+    rename(source = continent_aff,
+           target = continent_geoP) |> 
+    mutate(target = paste(target, " ", sep=""))
+  
+  nodes_conti <- data.frame(name=c(as.character(data_aff_geoP_conti$source), as.character(data_aff_geoP_conti$target)) %>% unique())
+  
+  data_aff_geoP_conti <- data_aff_geoP_conti |> 
+    mutate(# With networkD3, connection must be provided using id, not using real name like in the links dataframe.. So we need to reformat it.
+      IDsource = match(source, nodes_conti$name)-1,
+      IDtarget = match(target, nodes_conti$name)-1)
+  
+  ## --- Format data at the region scale
+  data_aff_geoP_region <- data_aff_geoP |> 
+    dplyr::select(analysis_id, region_geoP, region_aff) |> 
+    group_by(region_aff, region_geoP) |> 
+    summarise(value = n()) |> 
+    rename(source = region_aff,
+           target = region_geoP) |> 
+    mutate(target = paste(target, " ", sep=""))
+  
+  nodes_region <- data.frame(name=c(as.character(data_aff_geoP_region$source), as.character(data_aff_geoP_region$target)) %>% unique())
+  
+  data_aff_geoP_region <- data_aff_geoP_region |> 
+    mutate(# With networkD3, connection must be provided using id, not using real name like in the links dataframe.. So we need to reformat it.
+           IDsource = match(source, nodes_region$name)-1,
+           IDtarget = match(target, nodes_region$name)-1)
 
+      
+
+### ---- Plot Sankey Diagramm
+library(patchwork)
+library(hrbrthemes)
+library(circlize)
+library(networkD3)
+library(webshot)
+
+  
+  ## --- Continent scale
+  sankey_continent <- sankeyNetwork(Links = data_aff_geoP_conti, Nodes = nodes_conti,
+                                    Source = "IDsource", Target = "IDtarget",
+                                    Value = "value", NodeID = "name",
+                                    sinksRight = FALSE, nodeWidth = 40, fontSize = 30, nodePadding = 20) ; sankey_continent
+  
+  saveNetwork(sankey_continent, here::here("figures", "main", "sankey_continent.html"))
+  # you convert it as png
+  webshot(here::here("figures", "main", "sankey_continent.html"), 
+          here::here("figures", "main", "sankey_continent.png"), 
+          vwidth = 1000, vheight = 900)
+
+  ## --- Region scale
+  sankey_region <- sankeyNetwork(Links = data_aff_geoP_region, Nodes = nodes_region,
+                Source = "IDsource", Target = "IDtarget",
+                Value = "value", NodeID = "name", 
+                sinksRight = FALSE, nodeWidth = 40, fontSize = 30, nodePadding = 20) ; sankey_region
+  
+  saveNetwork(sankey_region, here::here("figures", "main", "sankey_region.html"))
+  # you convert it as png
+  webshot(here::here("figures", "main", "sankey_region.html"), 
+          here::here("figures", "main", "sankey_region.png"), 
+          vwidth = 1000, vheight = 900)
